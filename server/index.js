9449de74e5416d827db39c911d7bc37b35a6981e
AuthTextil <cryse.835@gmail.com>
2025-02-19 23:46:31 -0500

    feat (server): instalacion de axios y conexion a la base de datos

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mysql = require('mysql')
+
+const cors = require('cors')
+app.use(cors());
+app.use(express.json())
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'empleados_CRUD'
+})
+
+app.post('/create',(req, res)=>{
+    const nombre = req.body.nombre;
+    const edad = req.body.edad;
+    const pais = req.body.pais;
+    const cargo = req.body.cargo;
+    const tiempo_servicio = req.body.tiempo_servicio;
+
+    db.query('INSERT INTO empleados(nombre,edad,pais,cargo,tiempo_servicio) VALUES(?,?,?,?,?)', [nombre,edad,pais,cargo,tiempo_servicio],
+        (err, result)=>{
+            if(err){
+                console.log(err);
+            }else{
+                res.send('Empleado registrado con exito')
+            }
+        }
+    )
+})
+
+app.listen(3001,()=>{
+    console.log('corriendo en el puerton 3001')
+})
