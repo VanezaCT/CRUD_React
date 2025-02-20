de14c62abd1ce83cc11596bb0e8e05dc03d9e2c5
AuthTextil <cryse.835@gmail.com>
2025-02-20 01:12:50 -0500

    feat (style): instalacion de boostrap

--- server/index.js
+++ server/index.js
@@ -13,24 +13,42 @@ const db = mysql.createConnection({
     database: 'empleados_CRUD'
 })
 
-app.post('/create',(req, res)=>{
+app.post('/create', (req, res) => {
     const nombre = req.body.nombre;
     const edad = req.body.edad;
     const pais = req.body.pais;
     const cargo = req.body.cargo;
     const tiempo_servicio = req.body.tiempo_servicio;
 
-    db.query('INSERT INTO empleados(nombre,edad,pais,cargo,tiempo_servicio) VALUES(?,?,?,?,?)', [nombre,edad,pais,cargo,tiempo_servicio],
-        (err, result)=>{
-            if(err){
+    db.query('INSERT INTO empleados(nombre,edad,pais,cargo,tiempo_servicio) VALUES(?,?,?,?,?)', [nombre, edad, pais, cargo, tiempo_servicio],
+        (err, result) => {
+            if (err) {
                 console.log(err);
-            }else{
+            } else {
                 res.send('Empleado registrado con exito')
             }
         }
     )
 })
 
-app.listen(3001,()=>{
+app.get('/empleados', (req, res) => {
+    const nombre = req.body.nombre;
+    const edad = req.body.edad;
+    const pais = req.body.pais;
+    const cargo = req.body.cargo;
+    const tiempo_servicio = req.body.tiempo_servicio;
+
+    db.query('SELECT * FROM empleados',
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result)
+            }
+        }
+    )
+})
+
+app.listen(3001, () => {
     console.log('corriendo en el puerton 3001')
 })
